34066b1c60bdf2f9593e5d3049d09527c0a5c981
mati <mememiguelo@gmail.com>
2015-01-15 13:57:12 +0100

    proyectos que faltaban

---
+++ SpinnerFiguras/app/src/main/java/com/example/roberto/spinnerfiguras/Figuras.java
@@ -0,0 +1,21 @@
+package com.example.roberto.spinnerfiguras;
+
+/**
+ * Created by Miguel on 03/01/2015.
+ */
+
+public  class Figuras {
+    protected String tipo;
+
+
+    public Figuras(String t) {
+        tipo = t;
+
+    }
+
+    public String getTipo() {
+
+        return tipo;
+    }
+
+}
---
+++ SpinnerFiguras/app/src/main/java/com/example/roberto/spinnerfiguras/ViewHolder.java
@@ -0,0 +1,10 @@
+package com.example.roberto.spinnerfiguras;
+
+import android.widget.TextView;
+/**
+ * Created by Miguel on 03/01/2015.
+ */
+
+public class ViewHolder {
+    TextView tipo;
+}
